2a56376925fb447b8806b3d88420630cd32d46b7
lianaa98 <liana.kan98@gmail.com>
2023-04-03 16:34:20 -0700

    added semi colon

--- head.js
+++ head.js
@@ -10,7 +10,7 @@ const assertEqual = function(actual, expected) {
 
 const head = (array) => {
   return array[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
