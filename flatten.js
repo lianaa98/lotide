8ce016f027e17066aa1519e767aabb2e420a8315
lianaa98 <liana.kan98@gmail.com>
2023-04-04 16:46:03 -0700

    flatten.js

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = (array) => {
+
+  let flattenedArr = [];
+
+  for (let each of array) {
+    if (Array.isArray(each)) {
+      for (let item of each) {
+        flattenedArr.push(item);
+      }
+    } else {
+      flattenedArr.push(each);
+    }
+  }
+  return flattenedArr;
+};
