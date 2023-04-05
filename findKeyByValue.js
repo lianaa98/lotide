96fdbaa5b2fe71da5332f70b9da3fb676aa50fce
lianaa98 <liana.kan98@gmail.com>
2023-04-05 15:01:59 -0700

    findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const findKeyByValue = function(obj, val) {
+  for (let k in obj) {
+    if (obj[k] === val) return k;
+  }
+};
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
