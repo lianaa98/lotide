0ad5010e0be96a460cacb3a954d8688bf3dacf66
lianaa98 <liana.kan98@gmail.com>
2023-04-06 17:52:30 -0700

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,41 @@
+// takes in an object and a callback
+
+// callback = (x) => x.stars === 2 ---> returns true/false by comparing the key `stars` of object x
+
+const findKey = function(object, callback) {
+  for (let k in object) {
+    if(callback(object[k])) {
+      console.log(k);
+      return k;      
+    }
+  }
+  return;
+}
+
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+  
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
