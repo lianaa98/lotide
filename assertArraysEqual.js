495f6ab45a486b19227a58b294a8775a32ecf6b9
lianaa98 <liana.kan98@gmail.com>
2023-04-10 12:27:00 -0700

    refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,11 @@
-const eqArrays = (firstArr, secondArr) => {
-
-  if (firstArr.length !== secondArr.length) {
-    return false;
-  } else {
-    for (let i = 0; i < firstArr.length; i++) {
-      if (firstArr[i] !== secondArr[i]) return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (first, second) => {
-
   if(eqArrays(first, second)) {
     console.log(`✅✅✅ Assertion Passed: ${first} === ${second}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${first} !==  ${second}`);
+    return;
   }
+  console.log(`🛑🛑🛑 Assertion Failed: ${first} !==  ${second}`);
 };
+
+module.exports = assertArraysEqual;
