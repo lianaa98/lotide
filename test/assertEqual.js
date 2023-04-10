e596a578337b017331d42706a07c482952d22acb
lianaa98 <liana.kan98@gmail.com>
2023-04-10 12:16:46 -0700

    separate test codes from functions of head.js and assertEqual.js

--- test/assertEqual.js
+++
@@ -1,9 +0,0 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-    return;
-  }
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-};
-
-module.exports = assertEqual;
