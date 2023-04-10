baf5de6d25c0743e09d89172876f293399b39dcf
lianaa98 <liana.kan98@gmail.com>
2023-04-10 12:19:52 -0700

    refactored head.js and tail.js && separated test codes

--- head.js
+++ head.js
@@ -1,8 +1,5 @@
-const assertEqual = require('./test/assertEqual');
-
 const head = (array) => {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
--- tail.js
+++ tail.js
@@ -6,15 +6,4 @@ const tail = (array) => {
   return newArr;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
